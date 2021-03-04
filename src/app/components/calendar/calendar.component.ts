d0bcbed393aa17d93354ff716560942b27797ae9
hiJack666 <pushmeone@mail.ru>
2021-03-04 10:07:03 +0700

    правильное отображие дат и классов active

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -47,32 +47,22 @@ export class CalendarComponent implements OnInit {
         this.showThisMonth = 0;
     }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
+        await this.loadDatas();
         this.createYear(this.year);
     }
 
-    async createYear(year, clear = false) {
-        await this.loadDatas();
-        console.log('TYT', this.result);
-        console.log(year);
+    createYear(year, clear = false) {
+
         year = this.year;
         if (clear) {
             this.emptyDays = [];
             this.monthLengthArray = [];
             this.monthNames = [];
         }
-        for (let i = 0; i < this.yearLength; i++) {
+        for (let i = 0; i < this.yearLength; i++) { // цикл воссоздать в загрузке файла,
+        // и результат единожды записать в переменные, иначе при смене года массив увеличивается
             this.month = new Date(year, i).toLocaleString('ru', { month: 'long' });
-
-            this.monthActiveDays.push({activeDays: [], event: []});
-            // В месяце есть активные дни, если его массив имеет длину
-            if ( this.result[i].days.length ) {
-                this.result[i].days.filter(d => {
-                    this.monthActiveDays[i].activeDays.push(d.day);
-                    this.monthActiveDays[i].event.push(d.event);
-                }
-            );
-            }
 
             this.monthLength = new Date(year, i + 1, 0).getDate();
             this.monthLengthArray.push(this.monthLength);
@@ -86,6 +76,17 @@ export class CalendarComponent implements OnInit {
     // загружаем данные из json файла
     async loadDatas() {
         this.result = await this.electronService.loadData('data.json');
+        for (let i = 0; i < this.yearLength; i++) {
+            this.monthActiveDays.push({activeDays: [], event: [], year: Number || String });
+            // В месяце есть активные дни, если его массив имеет длину
+            if ( this.result[i].days.length ) {
+                this.result[i].days.filter(d => {
+                    this.monthActiveDays[i].activeDays.push(d.day);
+                    this.monthActiveDays[i].event.push(d.event);
+                    this.monthActiveDays[i].year = d.year;
+                });
+            }
+        }
         return this.result;
     }
 
